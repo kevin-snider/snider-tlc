284a7f4e6fa1d4e2d2da98dd924ab854981338c6
Kevin Snider <119872830+kevin-snider@users.noreply.github.com>
2026-01-22 11:03:27 -0500

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
 import Opportunities from './pages/Opportunities';
 
 // Scroll to top on route change
@@ -30,7 +29,6 @@ const App: React.FC = () => {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/gallery" element={<Gallery />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="/careers" element={<Opportunities />} />
           </Routes>
         </main>
